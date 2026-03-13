99338a3e03a908bad1e90f8b5436f1e180437873
unknown <janusz.kaniewski@outlook.com>
2026-03-13 21:37:02 +0100

    Don't move pipes when bird is dead

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -4,17 +4,21 @@ public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed = 5;
     public float deadZone = -30;
+    private BirdScript birdScript;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        birdScript = GameObject.FindGameObjectWithTag("Bird").GetComponent<BirdScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (birdScript.isBirdAlive)
+        {
+            transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        }
 
         if (transform.position.x < deadZone)
         {
