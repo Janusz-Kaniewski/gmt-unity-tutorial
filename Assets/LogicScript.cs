3ecf029347a8be19243d9114f63f9dec4a19e956
unknown <janusz.kaniewski@outlook.com>
2026-03-13 20:45:23 +0100

    Added game over canvas which displays when bird touches the pipe

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore = 0;
     public TextMeshProUGUI scoreText;
+    public GameObject gameOverScreen;
 
     [ContextMenu("Increase Score")]
     public void AddScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         scoreText.text = $"Score: {playerScore.ToString()}";
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
