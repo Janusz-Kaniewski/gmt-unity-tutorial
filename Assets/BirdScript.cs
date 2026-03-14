d1db089c78def8913b8c181bda48161333609e5c
unknown <janusz.kaniewski@outlook.com>
2026-03-14 14:32:18 +0100

    Added wings to the bird, and bird flaps the wings when user jumps

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -8,6 +8,13 @@ public class BirdScript : MonoBehaviour
     public LogicScript logicScript;
     public bool isBirdAlive = true;
 
+    public GameObject wingUp;
+    public GameObject wingDown;
+
+    private bool isWingsDown = false;
+
+    private float timer = 0;
+
     private InputAction jumpAction;
     private ParticleSystem particleSystem;
 
@@ -17,6 +24,8 @@ public class BirdScript : MonoBehaviour
         logicScript = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         particleSystem = GameObject.FindGameObjectWithTag("ParticleSystem").GetComponent<ParticleSystem>();
         jumpAction = InputSystem.actions.FindAction("Jump");
+
+        UpdateWings(isWingsDown);
     }
 
     // Update is called once per frame
@@ -25,7 +34,30 @@ public class BirdScript : MonoBehaviour
         if (jumpAction.IsPressed() && isBirdAlive)
         {
             myRigidbody.linearVelocity = Vector2.up * flapStrength;
+            isWingsDown = true;
         }
+
+        if (isWingsDown)
+        {
+            if (timer < 0.25)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer = 0;
+                isWingsDown = false;
+            }
+        }
+
+
+        UpdateWings(isWingsDown);
+    }
+
+    private void UpdateWings(bool isWingsDown)
+    {
+        wingDown.SetActive(isWingsDown);
+        wingUp.SetActive(!isWingsDown);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
